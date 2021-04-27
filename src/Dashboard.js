9e05dfcb62202075c746f06b29c3c92b05106064
valdcarl <77943334+valdcarl@users.noreply.github.com>
2021-04-27 01:53:22 -0600

    third commit

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,7 +1,78 @@
-import React from 'react';
+import { useState, useEffect } from 'react'
 import useAuth from "./useAuth"
+import Player from "./Player"
+import TrackSearchResult from './TrackSearchResult'
+import { Container, Form } from 'react-bootstrap'
+import SpotifyWebApi from 'spotify-web-api-node'
+
+const spotifyApi = new SpotifyWebApi({
+    cliendId: "d9200de3b99a4adf97d22e1077b7fb82",
+})
 
 export default function Dashboard({ code }) {
     const accessToken = useAuth(code)
-    return <div>{code}</div>
+    const [search, setSearch] = useState("")
+    const [searchResults, setSearchResults] = useState([])
+    const [playingTrack, setPlayingTrack] = useState()
+
+    function chooseTrack(track) {
+        setPlayingTrack(track)
+        setSearch('')
+    }
+
+    useEffect(() => {
+        if (!accessToken) return
+       spotifyApi.setAccessToken(accessToken)
+    }, [accessToken])
+
+    useEffect(() => {
+        if (!search) return setSearchResults([])
+        if (!accessToken) return
+
+        let cancel = false
+        spotifyApi.searchTracks(search).then(res => {
+            if (cancel) return
+            setSearchResults(res.body.tracks.items.map(track => {
+                const smallestAlbumImage = track.album.images.reduce((smallest,
+                image) => {
+                    if (image.height < smallest.height) return image
+                    return smallest
+                }, track.album.images[0])
+
+                return {
+                    artist: track.artists[0].name,
+                    title: track.name,
+                    uri: track.uri,
+                    albumUrl: smallestAlbumImage.url
+                }
+            }))
+        })
+
+        return () => cancel = true
+    }, [search, accessToken])
+
+    return (
+    <Container className='d-flex flex-column py-2' style={{
+        height: "100vh"}}>
+        <Form.Control
+            type='search'
+            placeholder="Search Songs/Artists"
+            value={search}
+            onChange={e => setSearch(e.target.value)}
+        />
+        <div className='flex-grow-1 my-2' style={{ overflowY:
+        'auto'}}>
+            {searchResults,map(track => (
+                <TrackSearchResult
+                    track={track}
+                    key={track.uri}
+                    chooseTrack={chooseTrack}
+                />
+            ))}
+        </div>
+        <div>
+            <Player accessToken={accessToken} trackUri={playingTrack?.uri} />
+        </div>
+    </Container>
+    )
 }
---
+++ src/Player.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import SpotifyPlayer from "react-spotify-web-playback"
+
+export default function Player( {accessToken, trackUri} ) {
+    if (!accessToken) return null
+    return <SpotifyPlayer
+        token={accessToken} 
+        showSaveIcon
+        uris={trackUri ? [trackUri] : []}
+    />
+}
